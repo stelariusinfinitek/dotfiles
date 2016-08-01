2e938760a06d75d9cec9431c17dc45343c6df54f
Gaman Gabriel <gamgabriel25@gmail.com>
2016-08-01 14:03:40 +0300

    Added the lock.sh script

---
+++ lock.sh
@@ -0,0 +1,9 @@
+#i3lock script, make it executable in /bin
+
+!/bin/bash
+ICON=$HOME/Pictures/lock.png
+TMPBG=/tmp/screen.png
+scrot /tmp/screen.png
+convert $TMPBG -scale 10% -scale 1000% $TMPBG
+convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+i3lock -u -i $TMPBG
